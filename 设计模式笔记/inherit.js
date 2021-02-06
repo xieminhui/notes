41706cd072f1f66a10c21b798e148f03062f8862
xie <xieminhui228@gmail.com>
2021-02-06 13:14:00 +0800

    M  js继承

--- 设计模式笔记/inherit.js
+++ 设计模式笔记/inherit.js
@@ -1,20 +1,22 @@
+
+
 // js 各种继承
 
-
-
 /**
- * 
+ * @description: 
  * 1. 原型链继承
  * 优点：可以继承父类的方法
  * 缺点： 父类的实列属性共享，不能向父类传参
- *
+ * @param {*}
+ * @return {*}
  */
+
 function Parent () {
-    this.parent = 'parent';
+    this.parents = ['parent1'];
 }
 
 Parent.prototype.getMyParent = function () {
-    return this.parent
+    return this.parents
 }
 
 function Child () {
@@ -26,8 +28,31 @@ Child.prototype = new Parent();
 
 const child1 = new Child();
 console.log(child1.getMyParent()); // parent
-child1.parent = 'parent change';
+child1.parents.push('parent2');
 
 const child2 = new Child();
-console.log(child1.getMyParent());// parent change
+console.log(child2.getMyParent());// parent change
 
+
+/**
+ * @description: 
+ * 2. 借用构造函数
+ * 优点：
+ * 缺点：
+ * @param {*}
+ * @return {*}
+ */
+
+function Parent (name) {
+    this.name = name;
+}
+
+function Child (name) {
+    Parent.call(this, name);
+}
+
+const child1 = new Child('joy');
+console.log(child1.name)
+
+const child2 = new Child('tom');
+console.log(child2.name)
