a91b18aa4976d6865d3aece647412a0e8de6329d
xieminhui <xmh19940228@163.com>
2021-03-05 17:26:51 +0800

    M 修改bind

--- jsFunction/bind.js
+++ jsFunction/bind.js
@@ -1,24 +1,52 @@
+
+Function.prototype.mycall = function(context) {
+    var context = context || window;
+    var fn = this;
+    var args = [...arguments].slice(1);
+    context.fn = fn;
+    var result = context.fn(...args);
+    delete context.fn;
+    return result;
+}
+
 Function.prototype.mybind = function(context) {
     var self = this;
     var args = [...arguments].slice(1);
-    return function () {
-        let newArgs = [...arguments];
-        // return self.apply(context, args.concat(newArgs))
-        var fn = self;
-        var allArgs = args.concat(newArgs);
-        context.fn = fn;
-        var result = context.fn(...allArgs);
-        delete context.fn;
-        return result;
+    function fBound () {
+        const allArgs = [...args, ...arguments];
+        return self.mycall(this instanceof fNOP ? this : context, ...allArgs);
     }
+
+    function fNOP () {
+
+    }
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+    return fBound;
 }
  
-const obj1 = {
-    "0": 1,
-    "1": 2,
-    "2": 3,
-    "length": 3
-  };
- 
-let res = Array.prototype.slice.mybind(obj1, 0);
-console.log(res()); 
+var value = 2;
+
+var foo = {
+    value: 1
+};
+
+function bar(name, age) {
+    this.habit = 'shopping';
+    console.log(this.value);
+    console.log(name);
+    console.log(age);
+}
+
+bar.prototype.friend = 'kevin';
+
+var bindFoo = bar.bind(foo, 'daisy');
+
+var obj = new bindFoo('18');
+// undefined
+// daisy
+// 18
+console.log(obj.habit);
+console.log(obj.friend);
+// shopping
+// kevin
