0e6d9548bbceb0df50ea911c1b8a4e12604df886
xieminhui <xmh19940228@163.com>
2021-03-10 10:36:55 +0800

    deepcopy

--- jsFunction/deepCopy.js
+++ jsFunction/deepCopy.js
@@ -1,26 +1,30 @@
 /*
  * @Date: 2021-03-01 10:52:28
  * @LastEditors: xieminhui
- * @LastEditTime: 2021-03-01 10:58:48
+ * @LastEditTime: 2021-03-10 10:29:31
  * @description: 
  */
 
- function deepCopy (obj) {
+ function deepCopy (obj, arr = []) {
    if(typeof obj !== 'object') return 0;
    const tmp = obj instanceof Array ? [] : {};
+   const index = arr.indexOf(obj);
+   if( index !== -1 ) return arr[index];
    for(let key in obj) {
      if(obj.hasOwnProperty(key)) {
        if(typeof obj[key] !== 'object') {
          tmp[key] = obj[key];
        }else {
-         tmp[key] = deepCopy(obj[key]);
+         arr.push(obj[key]);
+         tmp[key] = deepCopy(obj[key], arr);
        }
      }
    }
    return tmp;
  }
 
- const obj = {a:1, b:2,fn:function(){console.log('fn')}};
+ const obj = {a:1, b:2,fn:function(){console.log('fn')}, c: {d:1}};
+ obj.o = obj;
  const arr = [1,2];
  const copy1 = deepCopy(obj);
  const copy2 = deepCopy(arr);
@@ -30,3 +34,4 @@
  console.log(copy1, copy2)
  console.log('obj', 'arr');
  console.log(obj, arr)
+ console.log('end')
